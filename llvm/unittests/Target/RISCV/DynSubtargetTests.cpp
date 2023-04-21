0a51c97e01c3fb11a418776688b82469a7f2dfa7
Benedikt <bfj.koehn@gmail.com>
2023-04-21 12:18:31 +0200

    Used weird cmake hack to make the test not use the absolute paht

--- llvm/unittests/Target/RISCV/DynSubtargetTests.cpp
+++ llvm/unittests/Target/RISCV/DynSubtargetTests.cpp
@@ -2,8 +2,11 @@
 
 #include "gtest/gtest.h"
 
+#ifndef RISCV_SOURCE_FILE_DIR
+#error "Source file dir not defined"
+#endif
+
 TEST(DynSubtargetTests, TestForTestingTests) {
-    // TODO: make this not need to use absolute path
-    auto Test = llvm::getDynSubtargetData("/home/benedikt/projects/llvm-project/llvm/unittests/Target/RISCV/test_arch_defs.txt");
+    auto Test = llvm::getDynSubtargetData(RISCV_SOURCE_FILE_DIR "/test_arch_defs.txt");
     ASSERT_FALSE(true);
 }
